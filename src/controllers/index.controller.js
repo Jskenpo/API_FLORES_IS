8f62c1c9ba934d2490aa5e80dd73b945b2e8ef7b
jsantis <san21153@uvg.edu.gt>
2023-11-01 23:37:40 -0600

    pendpoints de arreglos creados

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,16 +4,38 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     password: 'ClavePOST#1',
-    database: 'Proyecto_IS',
+    database: 'floristeria',
     port: 5432,
 });
 
 
-const getArreglo = async (req, res) => {
+const getArregloByCategory = async (req, res) => {
+    const id = req.params.id;
     const consulta = `
-    SELECT a.Nombre, a.precio, a.id_categoria, c.categoria, a.foto
-    FROM Arreglo a
-    JOIN Categoria c ON a.id_categoria = c.id_categoria;`
+    SELECT a.nombre AS "Nombre del Arreglo",
+       a.SKU AS "SKU",
+       c.nombre AS "Nombre de la Categoría",
+       a.descripcion AS "Descripción",
+       a.precio AS "Precio"
+    FROM arreglo AS a
+    INNER JOIN categoria AS c ON a.id_categoria = c.id_categoria
+    where a.id_categoria = $1`
+
+    try{
+        const response = await pool.query(consulta, [id]);
+        const arreglo = response.rows;
+
+        console.log(arreglo);
+        res.status(200).json(arreglo);
+    }catch (e) {
+      console.log(e);
+      res.status(500).json({ error: 'Error en el servidor' });
+    }
+};
+
+const getCategories = async (req, res) => {
+    const consulta = `
+    SELECT * FROM categoria`
 
     try{
         const response = await pool.query(consulta);
@@ -25,9 +47,34 @@ const getArreglo = async (req, res) => {
       console.log(e);
       res.status(500).json({ error: 'Error en el servidor' });
     }
-};
+}
+
+const getArreglos = async (req, res) => {
+    const consulta = `
+    SELECT a.nombre AS "Nombre del Arreglo",
+       a.SKU AS "SKU",
+       c.nombre AS "Nombre de la Categoría",
+       a.descripcion AS "Descripción",
+       a.precio AS "Precio"
+    FROM arreglo AS a
+    INNER JOIN categoria AS c ON a.id_categoria = c.id_categoria`
+
+    try{
+        const response = await pool.query(consulta);
+        const arreglo = response.rows;
+
+        console.log(arreglo);
+        res.status(200).json(arreglo);
+    }catch (e) {
+      console.log(e);
+      res.status(500).json({ error: 'Error en el servidor' });
+    }
+}
+
 
 
 module.exports = {
-    getArreglo
+    getArregloByCategory,
+    getCategories,
+    getArreglos
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getArreglo } = require('../controllers/index.controller');
+const { getArregloByCategory, getCategories, getArreglos } = require('../controllers/index.controller');
 
-//GET 
-router.get('/arreglos', getArreglo);
+//GET
+router.get('/categorias', getCategories);
+router.get('/arreglos', getArreglos);
+
+//GET BY ID
+router.get('/arreglos/:id', getArregloByCategory);
 
 
 module.exports = router;
